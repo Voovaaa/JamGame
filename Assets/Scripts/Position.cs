2b6166a0d7759c7d5fe4da1becdae20b77dea85a
Voovaaa <rabotyagavova@gmail.com>
2026-02-07 14:05:50 +0300

    hi

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    Position connectedPosXP; // connected position on positive x axis
+    Position connectedPosXN;
+    Position connectedPosZP;
+    Position connectedPosZN;
+}
